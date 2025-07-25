64c144ce53ed77e3590696c8d0529d87586764fa
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-25 15:22:07 -0300

    chore: config jest config

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.spec.ts'],
+  roots: [
+    '<rootDir>/src',
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  clearMocks: true,
+  setupFiles: ['dotenv/config']
+};
+
+module.exports = config;
