6fa250330b8defeb254c09b57bae32a25386176e
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-26 17:44:31 -0300

    feat: implement loadUserAccountRepo in FacebookAuthenticationService

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,11 +1,19 @@
 import { type LoadFacebookUserApi } from '@/data/contracts/apis'
 import { AuthenticationError } from '@/domain/errors'
 import { type FacebookAuthentication } from '@/domain/features'
-
+import { type LoadUserAccountRepository } from '@/data/contracts/repos'
 export class FacebookAuthenticationService {
-  constructor (private readonly LoadFacebookUserApi: LoadFacebookUserApi) {}
+  constructor (
+    private readonly LoadFacebookUserApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepo: LoadUserAccountRepository
+  ) {}
+
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.LoadFacebookUserApi.loadUser(params)
+    const fbData = await this.LoadFacebookUserApi.loadUser(params)
+    if (fbData !== undefined) {
+      await this.loadUserAccountRepo.load({ email: fbData.email })
+    }
+
     return new AuthenticationError()
   }
 }
