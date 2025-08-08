9bf500f0fb1daf331d65a3de093b8203c928ce1a
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-08 17:39:51 -0300

    use endpoint to get user info

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,5 +1,6 @@
 import { type LoadFacebookUserApi } from '@/data/contracts/apis'
 import { type HttpGetClient } from '../http/client'
+import { access } from 'fs'
 
 export class FacebookApi {
   private readonly baseUrl = 'https://graph.facebook.com'
@@ -20,11 +21,19 @@ export class FacebookApi {
       }
     })
 
-    await this.httpClient.get({
+    const debugToken = await this.httpClient.get({
       url: `${this.baseUrl}/debug_token`,
       params: {
         access_token: appToken.access_token,
         input_token: params.token
+      }
+    })
+
+    await this.httpClient.get({
+      url: `${this.baseUrl}/${debugToken.data.user_id}`,
+      params: {
+        fields: ['id', 'name', 'email'].join(','),
+        access_token: params.token
       }
     })
   }
