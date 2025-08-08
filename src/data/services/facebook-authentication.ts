866a0ff4a2b3e6ad974f7941b7dcd7e70aceafaa
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-08 17:08:03 -0300

    lint fix

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,7 +1,10 @@
 import { type LoadFacebookUserApi } from '@/data/contracts/apis'
 import { AuthenticationError } from '@/domain/errors'
 import { type FacebookAuthentication } from '@/domain/features'
-import { type SaveFacebookAccountRepository, type LoadUserAccountRepository } from '@/data/contracts/repos'
+import {
+  type SaveFacebookAccountRepository,
+  type LoadUserAccountRepository
+} from '@/data/contracts/repos'
 import { FacebookAccount } from '@/domain/models/facebook-account'
 import { type TokenGenerator } from '../contracts/crypto/token'
 import { AccessToken } from '@/domain/models'
@@ -9,17 +12,25 @@ import { AccessToken } from '@/domain/models'
 export class FacebookAuthenticationService {
   constructor(
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository,
+    private readonly userAccountRepo: LoadUserAccountRepository &
+      SaveFacebookAccountRepository,
     private readonly crypto: TokenGenerator
   ) { }
 
-  async perform(params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
+  async perform(
+    params: FacebookAuthentication.Params
+  ): Promise<FacebookAuthentication.Result> {
     const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
-      const accountData = await this.userAccountRepo.load({ email: fbData.email })
+      const accountData = await this.userAccountRepo.load({
+        email: fbData.email
+      })
       const fbAccount = new FacebookAccount(fbData, accountData)
       const { id } = await this.userAccountRepo.saveWithFacebook(fbAccount)
-      const token = await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+      const token = await this.crypto.generateToken({
+        key: id,
+        expirationInMs: AccessToken.expirationInMs
+      })
       return new AccessToken(token)
     }
 
