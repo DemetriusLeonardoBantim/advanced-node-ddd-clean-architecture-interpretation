ad1f77c4e7fe20517b31eb07e269f3c87bfebc99
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-08 17:33:51 -0300

    use endpoint to debug_token

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -11,12 +11,20 @@ export class FacebookApi {
   ) { }
 
   async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({
+    const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.client_id,
         client_secret: this.client_secret,
         grant_type: 'client_credentials'
+      }
+    })
+
+    await this.httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
+      params: {
+        access_token: appToken.access_token,
+        input_token: params.token
       }
     })
   }
