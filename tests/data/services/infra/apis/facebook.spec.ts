ce6363e1ab7918f3bf83948a9c90fa3c54e9e726
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-08 17:07:53 -0300

    test: should get app token

--- tests/data/services/infra/apis/facebook.spec.ts
+++ tests/data/services/infra/apis/facebook.spec.ts
@@ -1,18 +1,33 @@
 import { type LoadFacebookUserApi } from '@/data/contracts/apis'
+import { mock } from 'jest-mock-extended'
 
 class FacebookApi {
+  constructor(private readonly httpClient: HttpGetClient) { }
   async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
+    await this.httpClient.get({
+      url: 'https://graph.facebook.com/oauth/access_token'
+    })
+  }
+}
 
+interface HttpGetClient {
+  get: (params: HttpGetClient.Params) => Promise<void>
+}
+
+namespace HttpGetClient {
+  export type Params = {
+    url: string
   }
 }
 
 describe('FacebookApi', () => {
   it('should get app token', async () => {
-    const sut = new FacebookApi()
+    const httpClient = mock<HttpGetClient>()
+    const sut = new FacebookApi(httpClient)
 
     await sut.loadUser({ token: 'any_client_token' })
 
-    expect(httpClient.get).toHaveBeenNthCalledWith({
+    expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oauth/access_token'
     })
   })
