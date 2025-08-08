40320eed44171b9efdf00e84895fe8d1e8a2ca40
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-08 17:25:37 -0300

    move facebook-account to corect folder

--- tests/data/services/infra/apis/facebook.spec.ts
+++ tests/data/services/infra/apis/facebook.spec.ts
@@ -1,37 +1,6 @@
-import { type LoadFacebookUserApi } from '@/data/contracts/apis'
+import { FacebookApi } from '@/infra/apis/facebook'
+import { type HttpGetClient } from '@/infra/http/client'
 import { mock } from 'jest-mock-extended'
-
-class FacebookApi {
-  private readonly baseUrl = 'https://graph.facebook.com'
-  constructor(
-    private readonly httpClient: HttpGetClient,
-    private readonly client_id: string,
-    private readonly client_secret: string
-
-  ) { }
-
-  async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({
-      url: `${this.baseUrl}/oauth/access_token`,
-      params: {
-        client_id: this.client_id,
-        client_secret: this.client_secret,
-        grant_type: 'client_credentials'
-      }
-    })
-  }
-}
-
-interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>
-}
-
-namespace HttpGetClient {
-  export type Params = {
-    url: string
-    params: object
-  }
-}
 
 describe('FacebookApi', () => {
   let clientId = 'any_client_id'
