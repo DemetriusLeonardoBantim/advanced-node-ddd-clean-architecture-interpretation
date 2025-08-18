8d8656a73ee680a03d40b4c10e28ab8087665ecd
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-18 09:18:36 -0300

    ensure JwtTokenGenerator rethws if sign throws

--- tests/data/services/infra/crypto/jwt-token-generator.spec.ts
+++ tests/data/services/infra/crypto/jwt-token-generator.spec.ts
@@ -48,4 +48,14 @@ describe("JwtTokenGenerator", () => {
     console.log(token);
     expect(token).toBe("any_token");
   });
+
+  it("Should rethrow if sign throws", async () => {
+    fakeJwt.sign.mockImplementationOnce(() => {
+      throw new Error("token_error");
+    });
+
+    const promise = sut.generateToken({ key: "any_key", expirationInMs: 1000 });
+
+    await expect(promise).rejects.toThrow(new Error("token_error"));
+  });
 });
