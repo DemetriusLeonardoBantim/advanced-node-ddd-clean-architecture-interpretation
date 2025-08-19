c4c5805d8048a67bcfbd9382b8ff0761b12e0963
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-18 21:58:39 -0300

    implement PgUser

--- tests/infra/postgress/repos/user-account.spec.ts
+++ tests/infra/postgress/repos/user-account.spec.ts
@@ -1,10 +1,37 @@
 import { type LoadUserAccountRepository } from "@/data/contracts/repos";
 import { newDb } from "pg-mem";
+import { Column, Entity, getRepository, PrimaryGeneratedColumn } from "typeorm";
 
 class PgUserAccountRepository implements LoadUserAccountRepository {
   async load(
     params: LoadUserAccountRepository.Params
-  ): LoadUserAccountRepository.Result {}
+  ): LoadUserAccountRepository.Result {
+    const pgUserRepo = getRepository(PgUser);
+
+    const pgUser = await pgUserRepo.findOne({ email: params.email });
+
+    if (pgUser !== undefined) {
+      return {
+        id: pgUser.id?.toString(),
+        name: pgUser?.name,
+      };
+    }
+  }
+}
+
+@Entity({ name: "usuarios" })
+class PgUser {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ name: "nome", nullable: true })
+  name!: string;
+
+  @Column()
+  email!: string;
+
+  @Column({ name: "id_facebook", nullable: true })
+  facebookId?: string;
 }
 
 describe("PgUserAccountRepository", () => {
@@ -13,14 +40,16 @@ describe("PgUserAccountRepository", () => {
       const db = newDb();
       const connection = await db.adapters.createTypeormConnection({
         type: "postgres",
-        entities: [],
+        entities: [PgUser],
       });
 
       await connection.synchonize();
-
+      const pgUserRepo = getRepository(PgUser);
+      await pgUserRepo.save({ email: "" });
       const sut = new PgUserAccountRepository();
 
-      await sut.load({ email: "existing_email" });
+      const account = await sut.load({ email: "existing_email" });
+      expect(account).toEqual({ id: "1" });
     });
   });
 });
