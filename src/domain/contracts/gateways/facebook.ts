689b8b059b6c037c4f7b0323a97bd8dabd078c54
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-08-21 19:11:19 -0300

    feat: create LoadFacebookUser gateways

--- src/domain/contracts/gateways/facebook.ts
+++ src/domain/contracts/gateways/facebook.ts
@@ -1,12 +1,14 @@
 export interface LoadFacebookUser {
-  loadUser: (input: LoadFacebookUser.Input) => Promise<LoadFacebookUser.Output>
+  loadUser: (input: LoadFacebookUser.Input) => Promise<LoadFacebookUser.Output>;
 }
 
 export namespace LoadFacebookUser {
-  export type Input = { token: string }
-  export type Output = undefined | {
-    facebookId: string
-    email: string
-    name: string
-  }
+  export type Input = { token: string };
+  export type Output =
+    | undefined
+    | {
+        facebookId: string;
+        email: string;
+        name: string;
+      };
 }
